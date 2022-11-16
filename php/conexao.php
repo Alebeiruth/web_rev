79ba53c438733a0d6c48f349606f90a2d35be22b
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2022-11-16 17:28:02 -0300

    Update conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$con = mysqli_connect("localhost:3306", "root", "root", "banco_produto" );
+$con = mysqli_connect("localhost:3306", "root", "root", "banco_produto" );  //(1º endereço:porta, 2º usuario = root, 3º senha = root, 4º nome BD)
 
 
 
