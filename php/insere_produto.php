dc014f2218a0aa34d46ce49e02e75c75105cbd84
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2022-11-16 17:28:35 -0300

    Update insere_produto.php

--- php/insere_produto.php
+++ php/insere_produto.php
@@ -11,3 +11,16 @@ mysqli_query($con, "INSERT INTO banco_produtos(nome, descricao, preco) VALUES ('
 
 ?>
 
+// PARA INTERAGIR COM BD > $resultado = mysqli_query($con, "<query>");
+// PARA INSERIR no BD >  mysqli_query($con, "INSERT INTO usuario(cpf, nome) VALUES ('99999', 'Alan Brado')");
+// PARA SELECIONAR no BD > mysli_query($con, "SELECT * FROM usuario");
+/* PARA OBTER RESULTADOS DE UMA QUERY
+$registro = mysqli_fetch_assoc($resultado);
+$cont = 0;
+while($registro = mysqli_fetch_assoc($resultado)) {
+  $dados[$cont]["coluna"] = $registro["coluna"];
+  $cont++;
+*/
+// PARA FECHAR CONEXÃO > mysqli_close($conexao);
+// PARA RETORNA MSG PARA JS > $objetoJSON = json_encode($dados);
+// echo $objetoJSON;
